0b70b816e62cc9e92251808fa65f948e29bd13f3
Timothy Haas <tlhaas@gmail.com>
2024-12-11 07:02:23 -0500

    clean up commenting and delete unused code.

--- js/the-business.js
+++ js/the-business.js
@@ -1,65 +1,54 @@
-
 function observeOrderListPage() {
-
   
   // We can reuse the majority of this code for both the new and old UIs
-  // The only main differences are the: 1) which root node we're targeting 2) 
+  // The only main differences are: 
+  // 1) which root node we're initially targeting mutation observation
+  // 2) the selector for grabbing <tr> tags in the table
   
   let UI_VER = window.location.host == "sellerportal.tcgplayer.com" ? "NEW" : "OLD";
 
   let rootNode, trTargetString;
 
   if (UI_VER === "NEW"){
-    // On the new UI we need to target the body because the table doesn't exist at page load
-    rootNode = document.body;  
-    // The new UI shares a URI handle for both the Order List and Order Details page, 
-    // so we use this selector only target TRs that appear in DOM generated for the Order List view
-    trTargetString = ".sp-order-index-view tbody.tcg-table-body > tr";
+    rootNode = document.body;  // On the new UI we need to target the body because the table doesn't exist at page load
+    trTargetString = ".sp-order-index-view tbody.tcg-table-body > tr"; // we need to be this specific because this code also fires on the Orders Detail page due to a shared UI
   }  
   else {
-    // Old UI is shockingly less complicated.
-    rootNode = document.querySelector('table');
+    rootNode = document.querySelector('table'); // table exists at page load in the old UI, so we can target it
     trTargetString = "tbody > tr";
   }
-
 
   const mutationCallback = function(mutationsList, observer) {
     for (const mutation of mutationsList) {
     
       /* 
-      Okay so sometimes the SPA is adding new TR tags to the table, but 
-      other times it's targeting TD tags for updates. Further still, it's occassionally 
-      targeting SPAN tags inside of TD tags for updates.
+      Okay. Sometimes the SPA is adding new <tr> tags to the table, but 
+      other times it's targeting <td> tags for updates. Further still, it's occassionally 
+      targeting <span> tags inside of <td> tags for updates.
 
       None of these elements, nor the table that contains them, exist on page load. 
 
-      Therefore, we must target the BODY and just watch for ALL childList changes (add/remove nodes),
-      and each time that happens, we have to look for ALL of the TR tags, pull the data, and update accordingly.
+      Therefore, we must target the <body> and just watch for ALL childList changes (add/remove nodes)
+      and each time that happens we have to look for ALL of the <tr> tags, pull the data, and update accordingly.
 
-      I hate this, but it's the most straight forward way I can see to do this, though I am a myopic individual.
-      If we target TR mutations directly we miss critical updates to the table or have to get very specific 
-      (read: rigid and complicated) with our mutation targeting and that seems like a rabbit hole down which 
-      I have no interest going. (Is that proper grammar?)
+      I hate this, but it's the most straight forward way I can see to do this, but to be fair I am also a myopic individual.
+      If we target <tr> mutations directly we miss critical updates to the table due to element reuse
+      or have to get very specific (read: rigid and complicated) with our mutation targeting 
+      and that seems like a rabbit hole down which I have no interest going. (Is that proper grammar?)
       */
 
-      
       if(mutation.type === 'childList'){  
 
         const allTR = rootNode.querySelectorAll(trTargetString);
         
-        // sometimes there are 0 TRs in the SPA is injecting into the DOM
-        // so we don't want to run this part  
+        // sometimes there are 0 <tr> tags while the SPA is injecting into the DOM, so we don't want to run this part  
         if (allTR.length > 0) { 
           allTR.forEach(tr => {
 
-            // for a brief moment in time there will be 1 TR will precisely 1 TD called `td.tcg-table-body__loading-cell`
-            // this indicates that the table contents are loading
-            // and we don't want to run this code while that's happening
+            // for a brief moment in time there will be 1 <tr> with precisely 1 <td> called `td.tcg-table-body__loading-cell`
+            // this indicates that the table contents are loading and we don't want to run this code while that's happening
 
             if (tr.childElementCount > 1){
-              // variables for efficiency
-              const parentnode  = tr.parentElement;
-              const trclass     = tr.className;
 
               // get the row data
               const tdElements    = tr.querySelectorAll('td');
@@ -68,7 +57,7 @@ function observeOrderListPage() {
               const shippingType  = tdElements[6].innerText;
               const orderTotal    = parseFloat(tdElements[9].innerText.replace('$', '')); 
 
-              // remove the taco classes from all <tr> elements because the <tr>s get reused by the SPA code
+              // remove the taco classes from all <tr> elements because they get reused by the SPA code
               const tacoClasses = ["taco-expedited","taco-intl","taco-bmwt","taco-pickup","taco-presale","taco-ready-for-pickup"];  
               tacoClasses.forEach(
                 (tacoClass) => {
@@ -101,8 +90,6 @@ function observeOrderListPage() {
   });
 
 } // end observeOrderListPage
-
-
 
 // Run all of this business
 observeOrderListPage();
